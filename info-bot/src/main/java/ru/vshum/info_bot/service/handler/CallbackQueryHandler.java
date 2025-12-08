81818448cd16d658649ca0e23a2b5ec3fbfecc6b
Vlad <vshum44@yandex.ru>
2025-12-08 22:19:58 +0300

    Create UpdateHandlers

--- info-bot/src/main/java/ru/vshum/info_bot/service/handler/CallbackQueryHandler.java
+++ info-bot/src/main/java/ru/vshum/info_bot/service/handler/CallbackQueryHandler.java
@@ -1,9 +1,11 @@
 package ru.vshum.info_bot.service.handler;
 
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.vshum.info_bot.telegram.Bot;
 
+@Service
 public class CallbackQueryHandler implements UpdateHandler {
     @Override
     public BotApiMethod<?> answer(Update update, Bot bot) {
--- info-bot/src/main/java/ru/vshum/info_bot/service/handler/CommandHandler.java
+++ info-bot/src/main/java/ru/vshum/info_bot/service/handler/CommandHandler.java
@@ -1,12 +1,39 @@
 package ru.vshum.info_bot.service.handler;
 
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.vshum.info_bot.telegram.Bot;
 
+@Service
 public class CommandHandler implements UpdateHandler {
     @Override
     public BotApiMethod<?> answer(Update update, Bot bot) {
-        return null;
+        String messageText = update.getMessage().getText();
+        return switch (messageText) {
+            case "/start" -> handleStart(update);
+            default -> defaultAnswer(update);
+        };
+    }
+
+    private BotApiMethod<?> handleStart(Update update) {
+        return SendMessage.builder()
+                .chatId(update.getMessage().getChatId())
+                .text("""
+                        Привет! Я информационный бот.
+                        Могу рассказать о погоде или курсах валют.
+                        """)
+                .build();
+    }
+
+    private BotApiMethod<?> defaultAnswer(Update update) {
+        return SendMessage.builder()
+                .chatId(update.getMessage().getChatId())
+                .text("""
+                        Я не знаю такой команды.
+                        Напиши /start, чтобы узнать мои возможности.
+                        """)
+                .build();
     }
 }
